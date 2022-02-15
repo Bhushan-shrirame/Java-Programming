8b1a2e8431510bbc08c438c7da925eb2e5c55b41
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2022-02-15 22:01:57 +0530

    .

--- helloWorld.java
+++ helloWorld.java
@@ -1,4 +1,7 @@
+//class Main{} - this is class
 class HelloWorld {
+    
+    //void Main(){] - This is a function
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
     }
