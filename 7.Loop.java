7db5afdcc181b962d619a0585163bbf72ecd4c7a
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2022-02-18 20:35:07 +0530

    Update 7.Loop.java

--- 7.Loop.java
+++ 7.Loop.java
@@ -3,38 +3,38 @@ import java.util.*;
 class Loop {
     public static void main(String  arg[]){
         // counter++ => counter = counter + 1
-    //    for(int counter = 0; counter < 3; counter = counter + 1){
-    //         System.out.println("Hello World");
-    //     }
-        // for(int i =0; i<10; i++){
-        //     System.out.println(i);
-        // }
+       for(int counter = 0; counter < 3; counter = counter + 1){
+            System.out.println("Hello World");
+        }
+        for(int i =0; i<10; i++){
+            System.out.println(i);
+        }
 
-        // // while loop
-        // int i = 0;
-        // while(i<11){
-        //     System.out.println(i);
-        //     i = i + 1 ; // OR i++
-        // }
+        // while loop
+        int i = 0;
+        while(i<11){
+            System.out.println(i);
+            i = i + 1 ; // OR i++
+        }
 
 
         // // do while loop
-        // int i = 0;
-        // do{
-        //     System.out.println(i);
-        //     i = i +1;
-        // }while(i<11);
+        int i = 0;
+        do{
+            System.out.println(i);
+            i = i +1;
+        }while(i<11);
 
         // print the sum of first n natural number 
 
-        // Scanner sc = new Scanner(System.in);
-        // int n = sc.nextInt();
-        // int sum =0;
-        // for(int i=1;i <= n;i++ ){
-        //     sum = sum +i;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum =0;
+        for(int i=1;i <= n;i++ ){
+            sum = sum +i;
             
-        // }
-        // System.out.println(sum);
+        }
+        System.out.println(sum);
 
         // print the table of a number input by the user
         
