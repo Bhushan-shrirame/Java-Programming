ba399953d082ab2edd94ad6fb8ee85622007f95a
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2022-02-18 21:13:25 +0530

    .

--- Function.java
+++ Function.java
@@ -2,16 +2,30 @@ import java.util.Scanner;
 
 public class Function {
     
-    public static void printMyName(String name){ 
-        System.out.println(name);
-        return;  
+    // public static void printMyName(String name){ 
+    //     System.out.println(name);
+    //     return; 
+    //}
+    // public static void main(String args[]){
+    //     Scanner sc = new Scanner(System.in);
+    //     String name = sc.nextLine();
+    //     printMyName(name); // call iya functon ko
 
+    // }
+
+
+
+    public static int calculatesum(int a, int b){
+        int sum = a+b;
+        return sum;
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
         
-        printMyName(name); // call iya functon ko
+        int sum = calculatesum(a,b);
+        System.out.println("Sum of 2 numbers is : "+sum);
 
     }
     
