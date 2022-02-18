b0619ac870a271ef47212cdd0436ee4728d93f63
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2022-02-18 21:33:42 +0530

    .

--- Function.java
+++ Function.java
@@ -15,19 +15,57 @@ public class Function {
 
 
 
-    public static int calculatesum(int a, int b){
-        int sum = a+b;
-        return sum;
-    }
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+    // public static int calculatesum(int a, int b){
+    //     int sum = a+b;
+    //     return sum;
+    // }
+    // public static void main(String args[]){
+    //     Scanner sc = new Scanner(System.in);
+    //     int a = sc.nextInt();
+    //     int b = sc.nextInt();
         
-        int sum = calculatesum(a,b);
-        System.out.println("Sum of 2 numbers is : "+sum);
+    //     int sum = calculatesum(a,b);
+    //     System.out.println("Sum of 2 numbers is : "+ sum);
+
+    // }
+
+
+    // for product of 2 number 
+
+    // public static int calculatemultiply(int a, int b){
+    //     return a*b;
+    // }
+    // public static void main(String args[]){
+    //     Scanner sc = new Scanner(System.in);
+    //     int a = sc.nextInt();
+    //     int b = sc.nextInt();
+    //     System.out.println("multiply of 2 numbers is : "+ calculatemultiply(a,b));
+
+    // }
+
+    //Factorial
+    public static void Factorial(int n ){
+        //loop
+        if(n<0){
+            System.out.println("Invalid Number");
+            return;
+        }
+        int factorial =1;
+        for(int i = n; i>=1; i--){
+            factorial = factorial * i;
+        }
+        System.out.println(factorial);
+    }
+
+    public static void main(String args[]){
+        Scanner sc  = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        Factorial(n);
 
     }
+
+
     
     
 }
