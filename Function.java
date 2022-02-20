a5c87983f193e152934bc0985dece8725c52fde2
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2022-02-20 21:27:25 +0530

    Update Function.java

--- Function.java
+++ Function.java
@@ -2,46 +2,46 @@ import java.util.Scanner;
 
 public class Function {
     
-    // public static void printMyName(String name){ 
-    //     System.out.println(name);
-    //     return; 
-    //}
-    // public static void main(String args[]){
-    //     Scanner sc = new Scanner(System.in);
-    //     String name = sc.nextLine();
-    //     printMyName(name); // call iya functon ko
+    public static void printMyName(String name){ 
+        System.out.println(name);
+        return; 
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        printMyName(name); // call iya functon ko
 
-    // }
+    }
 
 
 
-    // public static int calculatesum(int a, int b){
-    //     int sum = a+b;
-    //     return sum;
-    // }
-    // public static void main(String args[]){
-    //     Scanner sc = new Scanner(System.in);
-    //     int a = sc.nextInt();
-    //     int b = sc.nextInt();
+    public static int calculatesum(int a, int b){
+        int sum = a+b;
+        return sum;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
         
-    //     int sum = calculatesum(a,b);
-    //     System.out.println("Sum of 2 numbers is : "+ sum);
+        int sum = calculatesum(a,b);
+        System.out.println("Sum of 2 numbers is : "+ sum);
 
-    // }
+    }
 
 
     // for product of 2 number 
 
-    // public static int calculatemultiply(int a, int b){
-    //     return a*b;
-    // }
-    // public static void main(String args[]){
-    //     Scanner sc = new Scanner(System.in);
-    //     int a = sc.nextInt();
-    //     int b = sc.nextInt();
-    //     System.out.println("multiply of 2 numbers is : "+ calculatemultiply(a,b));
+    public static int calculatemultiply(int a, int b){
+        return a*b;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("multiply of 2 numbers is : "+ calculatemultiply(a,b));
 
-    // }
+    }
 
     //Factorial
     public static void Factorial(int n ){
