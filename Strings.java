f9a0ba7ac9b86641f62ed2b31e159b1b62bb62cf
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2022-02-23 21:20:49 +0530

    .

--- Strings.java
+++ Strings.java
@@ -1,10 +1,19 @@
+import java.util.*;
+
 public class Strings {
 
     public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
         //String Declaration
-        String name = "Tony";
-        String fullname = "Tony Stark";
-        String sentence = "My name is tony stark";
-    }
+        // String name = "Tony";
+        // String fullname = "Tony Stark";
+        // String sentence = "My name is tony stark";
+
+        String name =sc.nextLine();
+        System.out.println("Your name is : "+ name);
+
+
+        
+    } 
     
 }
