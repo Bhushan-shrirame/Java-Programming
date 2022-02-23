a99d56002c9f789452e5c7b130c295547aa37845
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2022-02-23 21:12:34 +0530

    .

---
+++ Strings.java
@@ -0,0 +1,10 @@
+public class Strings {
+
+    public static void main(String args[]){
+        //String Declaration
+        String name = "Tony";
+        String fullname = "Tony Stark";
+        String sentence = "My name is tony stark";
+    }
+    
+}
