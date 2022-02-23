683f8161bf367a03ab11062ad167211f07b91ec8
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2022-02-23 21:29:12 +0530

    .

--- Strings.java
+++ Strings.java
@@ -9,11 +9,22 @@ public class Strings {
         // String fullname = "Tony Stark";
         // String sentence = "My name is tony stark";
 
-        String name =sc.nextLine();
-        System.out.println("Your name is : "+ name);
+        // String name =sc.nextLine();
+        // System.out.println("Your name is : "+ name);
 
 
+        // Concatenation
+        String firstName = "Tony";
+        String lastName = "Stark";
+        String fullName = firstName + " " +lastName;
+        System.out.println(fullName);
+        System.out.println(fullName.length());
         
+        //charAt
+        for(int i=0; i<fullName.length(); i++){
+            System.out.println(fullName.charAt(i));
+        }
+
     } 
     
 }
