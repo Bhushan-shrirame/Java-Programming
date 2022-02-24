cf07770c83831682d867543021b18482872e455a
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2022-02-24 19:56:33 +0530

    .

---
+++ StingsBuilder.java
@@ -0,0 +1,27 @@
+class StingsBuilder {
+
+    // public static void main(Strings args[]){
+    public static void main(String args[]){
+
+        StringBuilder sb = new StringBuilder("Tony");
+        System.out.println(sb);
+
+        // cahr at index 0
+        System.out.println(sb.charAt(0));
+
+        //Set char at index  0
+        sb.setCharAt(0,'p');
+        System.out.println(sb);
+
+
+        sb.insert(2, 'n');
+        System.out.println(sb);
+
+        //delete the extra 'n'
+        sb.delete(2,3);
+        System.out.println(sb);
+
+
+    }
+    
+}
