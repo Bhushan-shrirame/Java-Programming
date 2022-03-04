4fd7eec5d1a9ffd1b13ccad431bdeac4d0fe8c85
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2022-03-04 16:49:30 +0530

    Update bits.java

--- bits.java
+++ bits.java
@@ -5,34 +5,34 @@ public class bits {
     public static void main(String args[]){
 
         // GET BIT
-        // int n = 5;
-        // int pos = 3;
-        // int bitMask = 1<<pos;
+        int n = 5;
+        int pos = 3;
+        int bitMask = 1<<pos;
 
-        // if((bitMask & n)==0) {
-        //     System.out.println("bit was zero ");
-        // }else {
-        //     System.out.println("bit was one");
-        // }
+        if((bitMask & n)==0) {
+            System.out.println("bit was zero ");
+        }else {
+            System.out.println("bit was one");
+        }
 
 
         //SET BIT
-        // int n = 5;
-        // int pos = 1;
-        // int bitMask = 1<<pos;
+        int n = 5;
+        int pos = 1;
+        int bitMask = 1<<pos;
 
-        // int newNumber = bitMask | n;
-        // System.out.println(newNumber);
+        int newNumber = bitMask | n;
+        System.out.println(newNumber);
 
 
         //Clear Bit
-        // int n = 5;
-        // int pos = 2;
-        // int bitMask = 1<<pos;
-        // int notBitMask = ~(bitMask);
+        int n = 5;
+        int pos = 2;
+        int bitMask = 1<<pos;
+        int notBitMask = ~(bitMask);
 
-        // int newNumber = notBitMask & n;
-        // System.out.println(newNumber);
+        int newNumber = notBitMask & n;
+        System.out.println(newNumber);
 
         // Update Bit
         Scanner sc = new Scanner(System.in);
