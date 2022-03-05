2b24a60aaf43d6c6c99e86784a52c1229b84ecbc
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2022-03-05 18:05:32 +0530

    .

--- Strings.java
+++ Strings.java
@@ -5,35 +5,35 @@ public class Strings {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         //String Declaration
-        // String name = "Tony";
-        // String fullname = "Tony Stark";
-        // String sentence = "My name is tony stark";
+        String name = "Tony";
+        String fullname = "Tony Stark";
+        String sentence = "My name is tony stark";
 
-        // String name =sc.nextLine();
-        // System.out.println("Your name is : "+ name);
+        String name =sc.nextLine();
+        System.out.println("Your name is : "+ name);
 
 
         // Concatenation
-        // String firstName = "Tony";
-        // String lastName = "Stark";
-        // String fullName = firstName + " " +lastName;
-        // System.out.println(fullName);
-        // System.out.println(fullName.length());
+        String firstName = "Tony";
+        String lastName = "Stark";
+        String fullName = firstName + " " +lastName;
+        System.out.println(fullName);
+        System.out.println(fullName.length());
         
         // //charAt
-        // for(int i=0; i<fullName.length(); i++){
-        //     System.out.println(fullName.charAt(i));
-        // }
+        for(int i=0; i<fullName.length(); i++){
+            System.out.println(fullName.charAt(i));
+        }
 
         //Compare
-        // String name1 ="Tony";
-        // String name2 = "Tony";
+        String name1 ="Tony";
+        String name2 = "Tony";
 
-        // if(name1.compareTo(name2) == 0){
-        //     System.out.println("String are equal");
-        // }else{
-        //     System.out.println("String are not equal");
-        // }
+        if(name1.compareTo(name2) == 0){
+            System.out.println("String are equal");
+        }else{
+            System.out.println("String are not equal");
+        }
 
         String sentence = "My name is Bhushan";
         String name = sentence.substring(11, sentence.length());
